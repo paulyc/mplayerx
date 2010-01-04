c6e9d48d99f2bf299e91f430a1a02557deff161b
zongyao.qu <zongyao.qu@cfc9e7b5-5070-499c-84c4-9901794ce5b6>
2010-01-04 08:39:10 +0000

    update package script

--- update/package.sh
+++ update/package.sh
@@ -9,10 +9,13 @@ cp -r ../MPlayerX/build/Release/MPlayerX.app ./MPlayerX.app
 zip -r MPlayerX.zip MPlayerX.app > /dev/null
 
 ruby GetTime.rb "./MPlayerX.zip"
+echo
 
 ruby GetBundleVersion.rb "./MPlayerX.app"
+echo
 
 ruby GetFileSize.rb "./MPlayerX.zip"
+echo
 
 echo "Sign:"
 ruby "../Sparkle.framework/Extras/Signing Tools/sign_update.rb" "./MPlayerX.zip" "../Sparkle.framework/Extras/Signing Tools/dsa_priv.pem"
