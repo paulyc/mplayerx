2f3903624f797a1c3870a31c7268923fd93a645e
zongyao.qu <zongyao.qu@cfc9e7b5-5070-499c-84c4-9901794ce5b6>
2010-02-21 10:23:36 +0000

    KVO相关的事情交给 CoreController seekable变成NSNumber

--- MPlayerX/MovieInfo.m
+++ MPlayerX/MovieInfo.m
@@ -22,27 +22,14 @@
 #import "coredef_private.h"
 #import "ParameterManager.h"
 
-@interface MovieInfo (LogAnalyzerDelegate)
--(void) logAnalyzeFinished:(NSDictionary*) dict;
-@end
-
-
-#define kMITypeFloat		(1)
-#define kMITypeBool			(2)
-#define kMITypeSubArray		(3)
-#define kMITypeSubAppend	(4)
-
 @implementation MovieInfo
 
 @synthesize demuxer;
 @synthesize chapters;
 @synthesize length;
 @synthesize seekable;
-
 @synthesize playingInfo;
-
 @synthesize metaData;
-
 @synthesize videoInfo;
 @synthesize audioInfo;
 @synthesize subInfo;
@@ -51,24 +38,11 @@
 {
 	if (self = [super init])
 	{
-		keyPathDict = [[NSDictionary alloc] initWithObjectsAndKeys:kKVOPropertyKeyPathCurrentTime, kMPCTimePos, 
-																   kKVOPropertyKeyPathLength, kMPCLengthID,
-																   kKVOPropertyKeyPathSeekable, kMPCSeekableID,
-																   kKVOPropertyKeyPathSubInfo, kMPCSubInfosID,
-																   kKVOPropertyKeyPathSubInfo, kMPCSubInfoAppendID,
-																   kKVOPropertyKeyPathCachingPercent, kMPCCachingPercentID,
-																   nil];
-		typeDict = [[NSDictionary alloc] initWithObjectsAndKeys:[NSNumber numberWithInt:kMITypeFloat], kMPCTimePos, 
-																[NSNumber numberWithInt:kMITypeFloat], kMPCLengthID,
-																[NSNumber numberWithInt:kMITypeBool], kMPCSeekableID,
-																[NSNumber numberWithInt:kMITypeSubArray], kMPCSubInfosID,
-																[NSNumber numberWithInt:kMITypeSubAppend], kMPCSubInfoAppendID,
-																[NSNumber numberWithInt:kMITypeFloat], kMPCCachingPercentID,
-																nil];
-		demuxer = nil;
-		chapters = nil;
-		length = nil;
-		seekable = NO;
+		NSNumber *zero = [NSNumber numberWithInt:0];
+		demuxer = [@"" retain];
+		chapters = [zero retain];
+		length = [zero retain];
+		seekable = [zero retain];
 		
 		playingInfo = [[PlayingInfo alloc] init];
 		metaData = [[NSMutableDictionary alloc] init];
@@ -81,12 +55,10 @@
 
 -(void) dealloc
 {
-	[keyPathDict release];
-	[typeDict release];
-
 	[demuxer release];
 	[chapters release];
 	[length release];
+	[seekable release];
 	[playingInfo release];
 	[metaData release];
 	[videoInfo release];
@@ -98,59 +70,24 @@
 
 -(void) resetWithParameterManager:(ParameterManager*)pm
 {
+	NSNumber *zero = [NSNumber numberWithInt:0];
+	
 	if (pm) {
 		[playingInfo resetWithParameterManager:pm];
 	}
+	
 	[metaData removeAllObjects];
 	[videoInfo removeAllObjects];
 	[audioInfo removeAllObjects];
 
-	[self setSeekable:NO];
-	[self setDemuxer:nil];
-	[self setChapters:nil];
-	[self setLength:nil];
+	[self setSeekable:zero];
+	[self setDemuxer:@""];
+	[self setChapters:zero];
+	[self setLength:zero];
 	
+	// 比较简单的实现KVO的方式，要不会一个一个的删除，效率比较低
 	[self willChangeValueForKey:@"subInfo"];
 	[subInfo removeAllObjects];
 	[self didChangeValueForKey:@"subInfo"];
-}
-
-// 这个是LogAnalyzer的delegate方法，
-// 因此是运行在工作线程上的，因为这里用到了KVC和KVO
-// 有没有必要运行在主线程上？
--(void) logAnalyzeFinished:(NSDictionary*) dict
-{
-	for (NSString *key in dict) {
-		NSString *keyPath = [keyPathDict objectForKey:key];
-		id obj;
-		
-		if (keyPath) {
-			//如果log里面能找到相应的key path
-			switch ([[typeDict objectForKey:key] intValue]) {
-				case kMITypeFloat:
-					[self setValue:[NSNumber numberWithFloat:[[dict objectForKey:key] floatValue]] forKeyPath:keyPath];
-					break;
-				case kMITypeBool:
-					[self setValue:[NSNumber numberWithBool:[[dict objectForKey:key] boolValue]] forKeyPath:keyPath];
-					break;
-				case kMITypeSubArray:
-					// 这里如果直接使用KVO的话，产生的时Insert的change，效率太低
-					// 因此手动发生KVO
-					[self willChangeValueForKey:@"subInfo"];
-					[subInfo setArray:[[dict objectForKey:key] componentsSeparatedByString:@":"]];
-					[self didChangeValueForKey:@"subInfo"];					
-					break;
-				case kMITypeSubAppend:
-					// 会发生insert的KVO change
-					obj = [[dict objectForKey:key] componentsSeparatedByString:@":"];
-					// NSLog(@"%@", obj);
-					[[self mutableArrayValueForKey:keyPath] addObject: [[obj objectAtIndex:0] lastPathComponent]];
-					break;
-
-				default:
-					break;
-			}
-		}
-	}
 }
 @end
