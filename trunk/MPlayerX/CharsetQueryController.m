a7c7ff99518c1e582c42d9eb205fcf6fca1d7f8e
zongyao.qu <zongyao.qu@cfc9e7b5-5070-499c-84c4-9901794ce5b6>
2010-03-21 07:40:58 +0000

    增加 charsetQueryController

---
+++ trunk/MPlayerX/CharsetQueryController.m
@@ -0,0 +1,77 @@
+/*
+ * MPlayerX - CharsetQueryController.m
+ *
+ * Copyright (C) 2009 Zongyao QU
+ * 
+ * MPlayerX is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * MPlayerX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MPlayerX; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#import "CharsetQueryController.h"
+#import "CocoaAppendix.h"
+#import "def.h"
+
+@implementation CharsetQueryController
+
++(void) initialize
+{
+	[[NSUserDefaults standardUserDefaults] registerDefaults:
+	 [NSDictionary dictionaryWithObjectsAndKeys:
+	  [NSNumber numberWithFloat:0.8], kUDKeyTextSubtitleCharsetConfidenceThresh,
+	  [NSNumber numberWithBool:YES], kUDKeyTextSubtitleCharsetManual,
+	  nil]];
+}
+
+-(id) init
+{
+	if (self = [super init]) {
+		nibLoaded = NO;
+	}
+	return self;
+}
+
+-(void) dealloc
+{	
+	[super dealloc];
+}
+
+-(CFStringEncoding) askForSubEncodingForFile:(NSString*)path charsetName:(NSString*)charsetName confidence:(float)conf
+{
+	if (!nibLoaded) {
+		nibLoaded = YES;
+		
+		[NSBundle loadNibNamed:@"SubEncoding" owner:self];
+		
+		[[charsetListPopup menu] removeAllItems];
+		[[charsetListPopup menu] appendCharsetList];
+	}
+	[outputText setStringValue:[NSString stringWithFormat:@"Detected file:%@\nEncoding:%@\nconfidence:%f", 
+								[path lastPathComponent], charsetName, conf]];
+	
+	return [NSApp runModalForWindow:encodingWindow];
+}
+
+-(IBAction) confirmed:(id)sender
+{
+	[NSApp stopModalWithCode:[[charsetListPopup selectedItem] tag]];
+	[encodingWindow orderOut:self];
+}
+
+-(IBAction) canceled:(id)sender
+{
+	[NSApp stopModalWithCode:kCFStringEncodingInvalidId];
+	[encodingWindow orderOut:self];
+}
+
+@end
