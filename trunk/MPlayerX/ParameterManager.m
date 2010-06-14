116e5a9d9e0f03e24564cbfdd5534cc9ccc273ea
zongyao.qu <zongyao.qu@cfc9e7b5-5070-499c-84c4-9901794ce5b6>
2010-06-14 13:11:20 +0000

    add options for mixtostereo

--- trunk/MPlayerX/ParameterManager.m
+++ trunk/MPlayerX/ParameterManager.m
@@ -401,7 +401,7 @@ NSString * const kPMValPan6To2			= @"pan=2:1:0:0:1:1:0:0:1:0.5:0.5:1:1";
 		[paramArray addObject:kPMParSTPause];
 	}
 	
-	// NSLog(@"%@", [paramArray componentsJoinedByString:@" "]);
+	// NSLog(@"%@", [paramArray componentsJoinedByString:@"\n"]);
 	
 	return paramArray;
 }
