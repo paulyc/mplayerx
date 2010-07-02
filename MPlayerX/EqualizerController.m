8787bb15bc0b47c1f9edf77667ac33d6c9cadc4b
zongyao.qu <zongyao.qu@cfc9e7b5-5070-499c-84c4-9901794ce5b6>
2010-07-02 12:25:33 +0000

    add code frame

--- MPlayerX/EqualizerController.m
+++ MPlayerX/EqualizerController.m
@@ -17,9 +17,14 @@
  * along with MPlayerX; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
- 
- 
+
+#import "def.h"
 #import "EqualizerController.h"
+#import "PlayerController.h"
+
+@interface EqualizerController (Internal)
+-(void) playBackWillStop:(NSNotification*)notif;
+@end
 
 @implementation EqualizerController
 
@@ -42,6 +47,10 @@
 		[menuEQPanel setKeyEquivalent:kSCMEqualizerPanelKeyEquivalent];
 		[menuEQPanel setKeyEquivalentModifierMask:kSCMEqualizerPanelKeyEquivalentModifierFlagMask];
 	}
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playBackWillStop:)
+												 name:kMPCPlayWillStopNotification object:playerController];
+
 }
 
 -(IBAction) showUI:(id)sender
@@ -49,6 +58,16 @@
 	if (!nibLoaded) {
 		nibLoaded = YES;
 	}
+}
+
+-(IBAction) setEqualizer:(id)sender
+{
+	[playerContrller setEqualizer:[sender cells]];
+}
+
+-(void) playBackWillStop:(NSNotification*)notif
+{
+	
 }
 
 @end
